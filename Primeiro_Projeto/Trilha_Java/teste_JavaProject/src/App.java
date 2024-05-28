aee34d6f30e962067fd01135ce7ac52197ade608
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-05-27 21:45:40 -0300

    Create App.java

---
+++ Primeiro_Projeto/Trilha_Java/teste_JavaProject/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi, eu fui executado pelo terminal");
+    }
+}
+
+/*
+ * Um projeto java criado pela IDE sempre tera uma pasta bin
+ * este é o nosso bytecode com arquivos .class
+ * 
+ */
