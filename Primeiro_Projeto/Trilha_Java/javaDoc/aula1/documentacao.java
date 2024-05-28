a50251dd55261c61e6465ef80af301dfe1d6513b
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-05-27 21:16:26 -0300

    Create documentacao.java

---
+++ Primeiro_Projeto/Trilha_Java/javaDoc/aula1/documentacao.java
@@ -0,0 +1,29 @@
+/* Documentação Java */
+
+/*
+ * Estudar a Documentação
+ * - Linguagem
+ * - Frameworks
+ */
+
+// Tipos de comentários = // única linha
+
+/*
+ * 
+ * Multi linhas
+ * 
+ */
+
+/**
+ * Documentação
+ */
+
+/*
+ * Os comentários servem de auxílio mas não para mitigar os problemas de uma
+ * estrtura de dados ruins
+ */
+
+// No terminal execute o comando abaixo
+// javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs aula1/*.java
+
+// Para gerar uma documentação
